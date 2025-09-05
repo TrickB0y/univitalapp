3260d0656345e3a560436182442ee4007672f23d
Marco Antônio R. A. B. Ribeiro <marcoantoniorabr@gmail.com>
2025-09-05 01:24:36 -0300

    a aplicação de contas foi criada e iniciada

---
+++ contas.py
@@ -0,0 +1,64 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from univitalapp.database import get_db # type: ignore
+
+
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+# página do registro
+@bp.route("/registro", methods=("GET", "POST"))
+def registro():
+    if request.method == "POST":
+        email = request.form["email"]
+        senha = request.form["senha"]
+        senha_confirmacao = request.form["senha_confirmacao"]
+        nome  = request.form["nome"]
+        sobrenome = request.form["sobrenome"]
+        telefone = request.form["telefone"]
+        db = get_db()
+        erro = None
+        
+        if not email:
+            erro = "O email é obrigatório."
+        elif not senha:
+            erro = "A senha é obrigatória."
+        elif not senha_confirmacao:
+            erro = "A senha deve ser confirmada."
+        elif not nome:
+            erro = "O nome é obrigatório."
+        elif not sobrenome:
+            erro = "O sobrenome é obrigatório."
+        elif not telefone:
+            erro = "O telefone é obrigatório."    
+        elif senha != senha_confirmacao:
+            erro = "As senhas não são iguais."
+            
+        if erro is None:
+            try:
+                db.execute(
+                    "INSERT INTO Usuarios (email, senha, nome, sobrenome, telefone) VALUES (?, ?, ?, ?, ?)",
+                    (email, generate_password_hash(senha), nome, sobrenome, telefone),
+                ).commit()
+            except db.IntegrityError:
+                erro = f"O Email {email} já esta cadastrado."
+            else:
+                return redirect(url_for("auth.login"))
+        # a função flash manda uma mensagem para o template de html em tempo de execução pro usuario.
+        # neste caso estamos mandando o erro cometido pelo usario no cadastro
+        flash(erro)
+        
+    return render_template("auth/registro.html")
+
+# to do: pagina de login
+
+# to do: função de sessão logada
+
+# to do: função de log out
+
+# to do: função de login obrigatorio
